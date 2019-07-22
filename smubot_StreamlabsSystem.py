8394e611a611bc0ce858f04a88d27d9b439b2c99
smu4242 <smu4242@users.noreply.github.com>
2019-07-22 23:27:18 +0200

    Fixed the fuzzy message about temperature + help output

--- smubot_StreamlabsSystem.py
+++ smubot_StreamlabsSystem.py
@@ -7,133 +7,14 @@ import os
 
 ScriptName = "Calculator"
 Website = "http://www.example.com/todo"
-Description = "Calculator for different temperature units (Celcius & Fahrenheit) and now also weight units (kg & lb)!"
+Description = "Calculator for different units (Celcius, Fahrenheit, kg, lbs, mi, km, inches & cm)"
 Creator = "smu4242"
-Version = "0.0.2"
+Version = "0.0.4"
 
 configFile = "config.json"
 settings = {}
 
-def ScriptToggled(state):
-    return
-
-def Init():
-    global settings
-
-    settings = {
-        "liveOnly": False,
-        "command": "!convert",
-        "permission": "Everyone",
-        "costs": 1,
-        "useCooldown": True,
-        "useCooldownMessages": True,
-        "showSassyMessage": True,
-        "cooldown": 1,
-        "userCooldown": 1,
-        "onCooldown": "$user, $command is still on cooldown for $cd minutes!",
-        "onUserCooldown": "$user $command is still on user cooldown for $cd minutes!",
-        "responseNotEnoughPoints": "$user you have only $points $currency to convert things smuDerp",
-        "response": u"$sourceValue$sourceUnit = $targetValue$targetUnit @$user $fuzzy"
-    }
-
-def Execute(data):
-    if data.IsChatMessage() and data.GetParam(0).lower() == settings["command"] and Parent.HasPermission(data.User, settings["permission"], "") and ((settings["liveOnly"] and Parent.IsLive()) or (not settings["liveOnly"])):
-        if len(data.GetParam(1).lower()) == 0:
-            Parent.SendStreamMessage("You need to provide the unit (temperature or weight), e.g. 42C, 4242F, 42kg, 42lb")
-            return
-
-        outputMessage = ""
-        userId = data.User
-        username = data.UserName
-        points = Parent.GetPoints(userId)
-
-        costs = settings["costs"]
-
-        if (costs > Parent.GetPoints(userId)) or (costs < 1):
-            outputMessage = settings["responseNotEnoughPoints"]
-        elif settings["useCooldown"] and (Parent.IsOnCooldown(ScriptName, settings["command"]) or Parent.IsOnUserCooldown(ScriptName, settings["command"], userId)):
-            if settings["useCooldownMessages"]:
-                if Parent.GetCooldownDuration(ScriptName, settings["command"]) > Parent.GetUserCooldownDuration(ScriptName, settings["command"], userId):
-                    cdi = Parent.GetCooldownDuration(ScriptName, settings["command"])
-                    cd = str(cdi / 60) + ":" + str(cdi % 60).zfill(2)
-                    outputMessage = settings["onCooldown"]
-                else:
-                    cdi = Parent.GetUserCooldownDuration(ScriptName, settings["command"], userId)
-                    cd = str(cdi / 60) + ":" + str(cdi % 60).zfill(2)
-                    outputMessage = settings["onUserCooldown"]
-                outputMessage = outputMessage.replace("$cd", cd)
-            else:
-                outputMessage = ""
-        else:
-            outputMessage = settings["response"]
-
-            if settings["useCooldown"]:
-                Parent.AddUserCooldown(ScriptName, settings["command"], userId, settings["userCooldown"])
-                Parent.AddCooldown(ScriptName, settings["command"], settings["cooldown"])
-
-        inputStr = data.GetParam(1).lower() + data.GetParam(2).lower()
-
-        sourceValue = 0
-        targetValue = 0
-        sourceUnit = "INVALID"
-        targetUnit = "INVALID"
-        celcius = 0
-        try:
-            if len(data.GetParam(1).lower()) == 0:
-                # Parent.SendStreamMessage("You need to provide the temperature, e.g. 42C or 4242F WHY HERE???")
-                return
-            last2 = inputStr[-2:]
-            last3 = inputStr[-3:]
-            exceptLast2 = inputStr[:-2]
-            exceptLast3 = inputStr[:-3]
-            if inputStr[len(inputStr)-1] == "c":
-                sourceValue = float(inputStr[0:len(inputStr)-1])
-                sourceUnit = u"\u00B0C"
-                targetUnit = u"\u00B0F"
-                targetValue = CtoF(sourceValue)
-                fuzzy = calcSassyMessage(sourceValue)
-            elif inputStr[len(inputStr)-1] == "f":
-                sourceValue = float(inputStr[0:len(inputStr)-1])
-                sourceUnit = u"\u00B0F"
-                targetUnit = u"\u00B0C"
-                targetValue = FtoC(sourceValue)
-                fuzzy = calcSassyMessage(targetValue)
-            elif last2 == "kg":
-                sourceValue = float(exceptLast2)
-                sourceUnit = "kg"
-                targetUnit = "lb"
-                targetValue = kgToLb(sourceValue)
-                fuzzy = ""
-            elif last2 == "lb":
-                sourceValue = float(exceptLast2)
-                sourceUnit = "lb"
-                targetUnit = "kg"
-                targetValue = lbToKg(sourceValue)
-                fuzzy = ""
-            elif last3 == "lbs":
-                sourceValue = float(exceptLast3)
-                sourceUnit = "lbs"
-                targetUnit = "kg"
-                targetValue = lbToKg(sourceValue)
-                fuzzy = ""
-            else:
-                Parent.SendStreamMessage("This did not work smuDerp Provide a number and then a unit (kg,lb,lbs,F,C) for example !convert 42kg")
-        except Exception, e:
-            outputMessage = "@$user Invalid input NotLikeThis Try $command 42C" + str(e)
-
-        outputMessage = outputMessage.replace("$sourceValue", str(sourceValue))
-        outputMessage = outputMessage.replace("$targetValue", str(targetValue))
-        outputMessage = outputMessage.replace("$sourceUnit", sourceUnit)
-        outputMessage = outputMessage.replace("$targetUnit", targetUnit)
-        outputMessage = outputMessage.replace("$cost", str(costs))
-        outputMessage = outputMessage.replace("$user", username)
-        outputMessage = outputMessage.replace("$points", str(points))
-        outputMessage = outputMessage.replace("$currency", Parent.GetCurrencyName())
-        outputMessage = outputMessage.replace("$command", settings["command"])
-        outputMessage = outputMessage.replace("$fuzzy", fuzzy)
-
-        Parent.SendStreamMessage(outputMessage)
-    return
+nothing = lambda x,y: ""
 
 def calcSassyMessage(celcius):
     if not settings["showSassyMessage"]:
@@ -174,18 +55,176 @@ def calcSassyMessage(celcius):
         return "Liar, liar pants on fire! This temperature does not even exist in this universe! NotLikeThis"
 
 kgToLbFactor = 2.20462
-
 def kgToLb(kg):
     return round(kg * kgToLbFactor, 2)
-
 def lbToKg(lb):
     return round(lb / kgToLbFactor, 2)
 
 def CtoF(celcius):
     return round((celcius * 1.8) + 32, 1)
+def FtoC(fahrenheit):
+    return round((fahrenheit - 32) / 1.8, 1)
 
-def FtoC(fahrenheid):
-    return round((fahrenheid - 32) / 1.8, 1)
+cmToInchFactor = 0.393701
+def cmToInch(cm):
+    return round(cm * cmToInchFactor, 1)
+def inchToCm(inch):
+    return round(inch / cmToInchFactor, 1)
+
+miToKmFactor = 1.6093
+def miToKm(mi):
+    return round(mi * miToKmFactor, 1)
+def kmToMi(km):
+    return round(km / miToKmFactor, 1)
+
+converterMap = {
+    "c": {
+        'sourceUnit': u"\u00B0C",
+        'targetUnit': u"\u00B0F",
+        'transformFun': CtoF,
+        'fuzzyFun': lambda x,y: calcSassyMessage(x)
+    },
+    "f": {
+        'sourceUnit': u"\u00B0F",
+        'targetUnit': u"\u00B0C",
+        'transformFun': FtoC,
+        'fuzzyFun': lambda x,y: calcSassyMessage(y)
+    },
+    "cm": {
+        'sourceUnit': "cm",
+        'targetUnit': "in",
+        'transformFun': cmToInch,
+        'fuzzyFun': nothing
+    },
+    "km": {
+        'sourceUnit': "km",
+        'targetUnit': "mi",
+        'transformFun': kmToMi,
+        'fuzzyFun': nothing
+    },
+    "mi": {
+        'sourceUnit': "mi",
+        'targetUnit': "km",
+        'transformFun': miToKm,
+        'fuzzyFun': nothing
+    },
+    "in": {
+        'sourceUnit': "in",
+        'targetUnit': "cm",
+        'transformFun': inchToCm,
+        'fuzzyFun': nothing
+    },
+    "kg": {
+        'sourceUnit': "kg",
+        'targetUnit': "lb",
+        'transformFun': kgToLb,
+        'fuzzyFun': nothing
+    },
+    "lb": {
+        'sourceUnit': "lbs",
+        'targetUnit': "kg",
+        'transformFun': lbToKg,
+        'fuzzyFun': nothing
+    }
+}
+# aliases:
+converterMap["lbs"] = converterMap["lb"]
+converterMap["''"] = converterMap["in"]
+
+def findConverter(inputStr):
+    for key in converterMap:
+        keyLength = len(key)
+        value = converterMap[key]
+        if inputStr[-keyLength:] == key:
+            sourceValue = float(inputStr[:-keyLength])
+            return value, sourceValue
+    return None, None
+
+def ScriptToggled(state):
+    return
+
+def Init():
+    global settings
+
+    settings = {
+        "liveOnly": False,
+        "command": "!convert",
+        "permission": "Everyone",
+        "costs": 1,
+        "useCooldown": True,
+        "useCooldownMessages": True,
+        "showSassyMessage": True,
+        "cooldown": 1,
+        "userCooldown": 1,
+        "onCooldown": "$user, $command is still on cooldown for $cd minutes!",
+        "onUserCooldown": "$user $command is still on user cooldown for $cd minutes!",
+        "responseNotEnoughPoints": "$user you have only $points $currency to convert things smuDerp",
+        "response": u"$sourceValue$sourceUnit = $targetValue$targetUnit @$user $fuzzy"
+    }
+
+def Execute(data):
+    if data.IsChatMessage() and data.GetParam(0).lower() == settings["command"] and Parent.HasPermission(data.User, settings["permission"], "") and ((settings["liveOnly"] and Parent.IsLive()) or (not settings["liveOnly"])):
+        if len(data.GetParam(1).lower()) == 0:
+            Parent.SendStreamMessage("You need to provide the unit (temperature or weight), e.g. 42C, 4242F, 42kg, 42lb")
+            return
+
+        outputMessage = ""
+        userId = data.User
+        username = data.UserName
+        points = Parent.GetPoints(userId)
+        costs = settings["costs"]
+
+        if (costs > Parent.GetPoints(userId)) or (costs < 1):
+            outputMessage = settings["responseNotEnoughPoints"]
+        elif settings["useCooldown"] and (Parent.IsOnCooldown(ScriptName, settings["command"]) or Parent.IsOnUserCooldown(ScriptName, settings["command"], userId)):
+            if settings["useCooldownMessages"]:
+                if Parent.GetCooldownDuration(ScriptName, settings["command"]) > Parent.GetUserCooldownDuration(ScriptName, settings["command"], userId):
+                    cdi = Parent.GetCooldownDuration(ScriptName, settings["command"])
+                    cd = str(cdi / 60) + ":" + str(cdi % 60).zfill(2)
+                    outputMessage = settings["onCooldown"]
+                else:
+                    cdi = Parent.GetUserCooldownDuration(ScriptName, settings["command"], userId)
+                    cd = str(cdi / 60) + ":" + str(cdi % 60).zfill(2)
+                    outputMessage = settings["onUserCooldown"]
+                outputMessage = outputMessage.replace("$cd", cd)
+            else:
+                outputMessage = ""
+        else:
+            outputMessage = settings["response"]
+            if settings["useCooldown"]:
+                Parent.AddUserCooldown(ScriptName, settings["command"], userId, settings["userCooldown"])
+                Parent.AddCooldown(ScriptName, settings["command"], settings["cooldown"])
+
+        fuzzy = ""
+        inputStr = data.GetParam(1).lower() + data.GetParam(2).lower()
+        try:
+            if len(data.GetParam(1).lower()) == 0:
+                # Parent.SendStreamMessage("You need to provide the temperature, e.g. 42C or 4242F WHY HERE???")
+                return
+            value, sourceValue = findConverter(inputStr)
+            if value is None:
+                Parent.SendStreamMessage("This did not work smuDerp Provide a number and then a unit (kg,lb,lbs,F,C,km,mi,cm,in) for example !convert 42kg")
+                return
+            sourceUnit = value['sourceUnit']
+            targetUnit = value['targetUnit']
+            targetValue = value['transformFun'](sourceValue)
+            fuzzy = value['fuzzyFun'](sourceValue, targetValue)
+        except Exception, e:
+            outputMessage = "@$user Invalid input NotLikeThis Try $command 42C Error: " + str(e)
+
+        outputMessage = outputMessage.replace("$sourceValue", str(sourceValue))
+        outputMessage = outputMessage.replace("$targetValue", str(targetValue))
+        outputMessage = outputMessage.replace("$sourceUnit", sourceUnit)
+        outputMessage = outputMessage.replace("$targetUnit", targetUnit)
+        outputMessage = outputMessage.replace("$cost", str(costs))
+        outputMessage = outputMessage.replace("$user", username)
+        outputMessage = outputMessage.replace("$points", str(points))
+        outputMessage = outputMessage.replace("$currency", Parent.GetCurrencyName())
+        outputMessage = outputMessage.replace("$command", settings["command"])
+        outputMessage = outputMessage.replace("$fuzzy", fuzzy)
+
+        Parent.SendStreamMessage(outputMessage)
+    return
 
 def ReloadSettings(jsonData):
     Init()
