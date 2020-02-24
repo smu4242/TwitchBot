09763fea5a5ee0cb21dafcea33108db91d99543f
smu4242 <smu4242@users.noreply.github.com>
2020-02-24 19:17:05 +0100

    Roles are assigned properly
    Even when not all roles exist already.
    Execution of !write and !s is still manual.
    
    Roles still have a random order.

--- DiscordRoleBot/updateranks.py
+++ DiscordRoleBot/updateranks.py
@@ -23,11 +23,17 @@ def role_exists(guild, role):
     return False
 
 
+def f1(r, name):
+    debug("f1: " + str(r))
+    return r.name == name
+
 def getRoleByName(guild, roleName):
-    for r in guild.roles:
-        if str(r) == roleName:
-            return r
-    return None
+    # for r in guild.roles:
+    #     if str(r) == roleName:
+    #         return r
+    # return None
+    # return discord.utils.find(lambda r: f1(r, name), guild.roles)
+    return discord.utils.get(guild.roles, name = roleName)
 
 
 def read_roles_json():
@@ -62,25 +68,25 @@ async def sync_roles(message):
         # debug("We got a member!" + str(member))
         discordRoles = {}
         for role in data['roles']:
-            debug("role in json exists? " + str(role_exists(message.guild, role['name'])))
+            # debug("role in json exists? " + str(role_exists(message.guild, role['name'])))
             roleName = role['name']
             discordRole = getRoleByName(message.guild, roleName)
+            # debug("found discord role? " + str(discordRole) + " for rank " + roleName)
             if not discordRole:
-                await message.guild.add_roles(name=role['name'], hoist=True, mentionable=False)
+                await message.guild.create_role(name=roleName, hoist=True, mentionable=False)
+                debug("created role " + roleName)
+                discordRole = getRoleByName(message.guild, roleName)
             discordRoles[roleName] = discordRole
         for user in data['users']:
             roleName = data['users'][user]
-            debug("user in streamlabs:" + user)
             discordMember = getMemberByName(message.guild, user)
             if discordMember:
                 discordRole = discordRoles[roleName]
-                debug("user in discord:" + str(discordMember) + " will get role " + str(discordRole))
                 await discordMember.add_roles(discordRole, reason="update ranks script 1")
-                debug("A")
-            debug("B")
-        debug("C")
+                debug("assigned member " + str(discordMember) + " to role " + str(discordRole))
+        debug("Done")
         await message.channel.send('Done!')
-    except Exception as e:
+    except e:
         debug("Unexpected error:", traceback.format_exc())
         sys.stdout.flush()
 
